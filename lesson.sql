39bf1bd0253a994eeb89a47da555a2d2cda13921
25k2ph8pfc-hub <25k2ph8pfc@privaterelay.appleid.com>
2026-06-18 11:23:52 +0000

    End of morning course

--- lesson.sql
+++ lesson.sql
@@ -1,8 +1,27 @@
 /*
-Example lesson stub
+My First SQL Lesson
+Author - Matthew Emery
+18/06/2026
 */
 
+-- Querying Surgical Activity
 SELECT
-	*
+	ps.PatientId
+	
+	,ps.Hospital
+	
+	,ps.Ward
+	
+	,DATEADD(WEEK, -2, ps.AdmittedDate) AS ReminderDate
+	
+	,ps.AdmittedDate
+	
+	,ps.DischargeDate
+	
+	,ps.Tariff
+
 FROM
-	PatientStay ps ;
+	PatientStay AS ps
+WHERE ps.Hospital IN ('Kingston','PRUH')
+	AND ps.Ward LIKE '%Surgery'
+	AND ps.AdmittedDate BETWEEN '2024-02-27' AND '2024-02-29'
